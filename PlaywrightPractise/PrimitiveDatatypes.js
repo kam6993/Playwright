96383ae1edcef071f7870b60b317937cd9dbd17e
Kamalesh Raj <165658074+kam6993@users.noreply.github.com>
2025-11-25 16:24:08 +0530

    Add files via upload

---
+++ PlaywrightPractise/PrimitiveDatatypes.js
@@ -0,0 +1,19 @@
+let firstName = "Kamalesh"
+console.log("The value of firstName is: " +firstName);
+console.log(typeof firstName);
+
+let companyName = null
+console.log(companyName);
+console.log(typeof companyName);
+
+let mobileNumber = 9444480983
+console.log(mobileNumber);
+console.log(typeof mobileNumber);
+
+let isAutomation = true
+console.log(isAutomation);
+console.log(typeof isAutomation);
+
+let hasPlaywright; 
+console.log(hasPlaywright);
+console.log(typeof hasPlaywright);
